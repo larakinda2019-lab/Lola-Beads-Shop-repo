be4c90a14776c9dcbc50b87766060cbcc104fbba
larakinda2019-lab <lara.kinda2019@gmail.com>
2025-08-17 17:12:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ const PORT = process.env.PORT || 3000;
 // Serve static files from the public folder
 app.use(express.static(path.join(__dirname, "public")));
 
+// Serve index.html for root
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
 
